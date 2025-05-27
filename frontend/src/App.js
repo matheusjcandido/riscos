6d46a0ea2cf93650d1c4ce98abf9fd2dea7bf63b
Matheus J. Candido <matheusjcandido@gmail.com>
2025-05-27 10:40:53 -0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Arquivo CSS para o App (opcional)
+import RiskMatrixApp from './RiskMatrixApp'; // Seu componente de Matriz de Risco
+
+function App() {
+  return (
+    <div className="App">
+      <RiskMatrixApp />
+    </div>
+  );
+}
+
+export default App;
